a842543b155afc7aca6fff39f9ac78806a7b77be
emadhussein3@gmail.com <emadhussein3@gmail.com>
2023-02-05 14:25:27 +0300

    Add project files.

---
+++ ToDoListApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ToDoListApp
+{
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Task> tasks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks = new ObservableCollection<Task>();
+            TasksList.ItemsSource = tasks;
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task { Description = TaskDescription.Text };
+            tasks.Add(task);
+            TaskDescription.Clear();
+        }
+
+        private void DeleteTask_Click(object sender, RoutedEventArgs e)
+        {
+            Task task = TasksList.SelectedItem as Task;
+            if (task != null)
+            {
+                tasks.Remove(task);
+            }
+        }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+    }
+}
